91c0fe3e2820eb1d1bc3151f438417cd84c01cd4
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-15 18:25:36 +0900

    add Viewer, frame

---
+++ src/components/Viewer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-html="value"></div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: {
+    value: {
+      type: String
+    }
+  }
+})
+export default class extends Vue {}
+</script>
+
---
+++ src/components/ViewerFrame.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>template heading</h1>
+        <slot>template body</slot>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class extends Vue {}
+</script>
