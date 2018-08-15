08732fd7508b376d1b5c1c36249e907c4306dec7
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-15 16:01:14 +0900

    add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <header>Header</header>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({})
+export default class extends Vue {}
+</script>
+
