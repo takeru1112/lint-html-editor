0a5bc2018b63cfc3badfc1ff26e9f6158764018f
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-15 16:01:23 +0900

    add Editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,20 +1,45 @@
 <template>
-  <div ref="container"></div>
+  <div>
+    <div :style="{width: width, height:height}" ref="container"></div>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import * as monaco from "monaco-editor";
 import IStandaloneCodeEditor = monaco.editor.IStandaloneCodeEditor;
 
-@Component({})
+@Component({
+  props: {
+    width: {
+      type: String,
+      default: "400px"
+    },
+    height: {
+      type: String,
+      default: "600px"
+    },
+    value: {
+      type: String,
+      default: ""
+    }
+  }
+})
 export default class extends Vue {
-  $refs: {
+  $refs!: {
     container: HTMLDivElement;
   };
+  editor!: IStandaloneCodeEditor;
+  value!: string;
   public mounted() {
-    monaco.editor.create(this.$refs.container, {
-      language: "html"
-    });
+    this.editor = monaco.editor.create(this.$refs.container);
+    const model = monaco.editor.createModel(this.value, "html");
+    this.editor.setModel(model);
+    this.editor.onDidChangeModelContent(this.onDidChange);
+  }
+  private onDidChange(e: monaco.editor.IModelContentChangedEvent) {
+    console.log(e);
+    const value = this.editor.getValue();
+    this.$emit("input", value);
   }
 }
 </script>
