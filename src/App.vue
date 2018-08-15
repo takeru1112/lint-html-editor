c4a80a0296c77c391af16c311ccb96b7d4740d32
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-15 18:25:23 +0900

    remove textlint

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app" class="container">
     <Editor v-model="value" @input="onChange" />
-    <ErrorLog :items="messages" />
-    <span v-html="value"></span>
+    <ViewerFrame>
+      <Viewer :value="value" />
+    </ViewerFrame>
   </div>
 </template>
 
@@ -10,56 +11,23 @@
 import { Component, Vue } from "vue-property-decorator";
 
 import Editor from "./components/Editor.vue";
-import ErrorLog from "./components/ErrorLog.vue";
-import {
-  TextlintResult,
-  TextlintKernel,
-  TextlintMessage
-} from "@textlint/kernel";
+import ViewerFrame from "./components/ViewerFrame.vue";
+import Viewer from "./components/Viewer.vue";
 
 @Component({
   components: {
     Editor,
-    ErrorLog
+    ViewerFrame,
+    Viewer
   }
 })
 export default class App extends Vue {
   value: string = "";
-  messages: TextlintMessage[] = [];
-  linter: TextlintKernel = new TextlintKernel();
-  public onChange(val: string) {
-    this.execLint(val).then((result: TextlintResult | null) => {
-      if (!result) {
-        return;
-      }
-      this.messages = result.messages;
-    });
-  }
-  private async execLint(text: string) {
-    const preset = await require("textlint-rule-preset-japanese");
-    const rules = Object.keys(preset.rules).map(name => {
-      return {
-        ruleId: name,
-        rule: preset.rules[name],
-        options: preset.rulesConfig[name]
-      };
-    });
-    const plugin = await require("textlint-plugin-html");
-    console.log(plugin);
-    try {
-      return this.linter.lintText(text, {
-        rules: rules,
-        plugins: [
-          {
-            pluginId: "html",
-            plugin
-          }
-        ],
-        ext: ".html"
-      });
-    } catch {
-      return null;
-    }
-  }
+  public onChange(val: string) {}
 }
 </script>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+}
+</style>
