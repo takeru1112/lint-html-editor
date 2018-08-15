5aa05790bc847e216a999bb2217aedc9662b9a4e
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-15 13:24:00 +0900

    add Editor component

---
+++ src/components/Editor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div ref="container"></div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import * as monaco from "monaco-editor";
+import IStandaloneCodeEditor = monaco.editor.IStandaloneCodeEditor;
+
+@Component({})
+export default class extends Vue {
+  $refs: {
+    container: HTMLDivElement;
+  };
+  public mounted() {
+    monaco.editor.create(this.$refs.container, {
+      language: "html"
+    });
+  }
+}
+</script>
