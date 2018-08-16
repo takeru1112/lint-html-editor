b910dda20a053c7e0c2dcb5ac9de09b936757afd
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-16 13:14:47 +0900

    rename pages Components

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import Vue from "vue";
-import App from "./pages/Index.vue";
-import "./registerServiceWorker";
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import BootstrapVue from "bootstrap-vue"
+import Index from "./pages/Index.vue";
+import "./registerServiceWorker";
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(Index)
 }).$mount("#app");
--- src/preview.ts
+++ src/preview.ts
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./pages/Preview.vue";
+import Preview from "./pages/Preview.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(Preview)
 }).$mount("#app");
