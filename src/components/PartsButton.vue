a7f6c2b7f94fb6365c51f8dfa810a12212ddec1a
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-16 14:23:20 +0900

    add Parts button components

---
+++ src/components/PartsButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-dropdown>
+    <b-dropdown-item v-for="(index,item) in items" :key="index">{{item.title}}</b-dropdown-item>
+  </b-dropdown>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: {
+    items: {
+      type: Array
+    }
+  }
+})
+export default class extends Vue {}
+</script>
+
---
+++ src/components/PartsButtonGroup.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-container>
+    <PartsButton />
+  </b-container>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import PartsButton from "./PartsButton.vue";
+
+@Component({
+  components: { PartsButton },
+  props: {
+    items: {
+      type: Array
+    }
+  }
+})
+export default class extends Vue {}
+</script>
+
