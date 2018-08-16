c3dac51d3d01cda02032397a5c11e2849084ced9
Wada Takeru <takeru.wada.1112@gmail.com>
2018-08-16 13:05:21 +0900

    split index,preview

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      template: "public/index.html"
+    },
+    preview: {
+      entry: "src/preview.ts",
+      template: "public/preview.html"
+    }
+  },
   configureWebpack: {
     plugins: [new MonacoWebpackPlugin()]
   }
